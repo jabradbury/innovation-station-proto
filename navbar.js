a65eb773b277b886b61ea2e7b8de9e3bc0423039
jabradbury <117648490+jabradbury@users.noreply.github.com>
2023-04-26 16:06:22 +0100

    removed title from navbar

--- navbar.js
+++ navbar.js
@@ -15,9 +15,6 @@ const Navbar = () => {
           className="navbar__search-input"
         />
       </div>
-      <div className="navbar__title-container">
-        <h1 className="navbar__title">Fleetcor UK</h1>
-      </div>
       <div className="navbar__button-container">
         <button className="navbar__button">Innovation Hub</button>
       </div>
