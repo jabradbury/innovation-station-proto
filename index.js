6a8615b449ea12144b65376fab07ea1e37f4e80d
jabradbury <117648490+jabradbury@users.noreply.github.com>
2023-04-26 15:57:36 +0100

    updated to be more concise

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Form from './form';
 import Modal from './modal';
+import Navbar from './navbar';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Form />
   </React.StrictMode>
 );
 
